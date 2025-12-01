ea3d23a56a74d359ef0812d7de372f6be32ae046
Amin <aminullahpuya@gmail.com>
2025-12-01 10:32:35 +0100

    pushing hangman

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+print("Welcome to Hangman!")
+words = ["hacker", "bounty", "python"]
+secret_word = random.choice(words)
+print(secret_word)
+display_word = []
+for letter in secret_word:
+    display_word += "-"
+print(display_word)
+
+game_over = False
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+    for position in range(len(secret_word)):
+        letter = secret_word[position]
+        if letter == guess:
+            display_word[position] = letter
+    print(display_word)
+
+    if "-" not in display_word:
+        print("Congratulations! You've guessed the word!")
+        game_over = True
